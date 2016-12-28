29a1d7f633893014c0bd6af368ddd16ff9a3a17a
Kilian <kilian@aklio.de>
2016-12-28 15:38:56 +0100

    creating new class

---
+++ src/FtpConnection/EmptyClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FtpConnection;
+
+/**
+ *
+ * @author MarkTheSmasher
+ */
+public class EmptyClass {
+    
+    /*Wichtige Funktionen:
+        * Konstruktor: FTP-Adresse, Username, Passwort --> Verbindung herstellen
+        * file.listFiles
+        * file.copyFileToServer
+        * file.copyFileFromServe
+              
+        */
+    
+}
